9bd8efa0b6b225fffc5100041fb4012987b4fd28
dhiraj <dhirajmymodak22@gmail.com>
2025-02-04 15:16:09 +0530

    pushed

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,22 +1,32 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/user.model');
-const ErrorHandler = require('../utils/Errorhandler');
-const asyncErrorHandler = require('./asyncErrorHandler');
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
+const ErrorHandler = require("../utils/Errorhandler");
+const asyncErrorHandler = require("./asyncErrorHandler");
 
-const isAuthenticatedUser =asyncErrorHandler(async(req,res,next)=>{
+const isAuthenticatedUser = asyncErrorHandler(async (req, res, next) => {
+  // const {token} = req.cookies;
+  // console.log(token);
 
-    const {token} = req.cookies;
-    console.log(token);
-    
-    if(!token){
-        return res.status(401).json({success:false,message:"Please login to access"});
-    }
+  const token =
+    req.headers.authorization && req.headers.authorization.split(" ")[1];
+  console.log(token);
 
-    const decoded = jwt.verify(token,process.env.JWT_SECRET);
+  if (!token) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Please login to access" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decoded.id);
+
     next();
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Invalid or expired token" });
+  }
+});
 
-})
-
-
-module.exports = {isAuthenticatedUser}
+module.exports = { isAuthenticatedUser };
